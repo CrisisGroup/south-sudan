4151626543ec0e46530e7a8733f77a33d7e61fb7
pkfranz <paulkfranz@gmail.com>
2022-08-22 10:20:26 -0400

    map cache refresh

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-    style: 'mapbox://styles/daltonwb/cl6z2v4u4000b14qr1e79vcsw',
+    style: 'mapbox://styles/daltonwb/cl74uh6bc000t15rwd524t235',
     accessToken: 'pk.eyJ1IjoiZGFsdG9ud2IiLCJhIjoiOWdSSXFQSSJ9.HZyjh4g3TAAOAncwelv9Vw',
     showMarkers: false,
     markerColor: '#3FB1CE',
